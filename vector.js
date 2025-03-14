5f49126265c214eb34c728e214c8051baa8f2b6e
Ben Payne <me@benpayne.dev>
2025-03-14 14:02:45 -0400

    Project 2

--- vector.js
+++ vector.js
@@ -1,42 +1,50 @@
 class Vector3 {
-  constructor(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-  scale(scalar) {
-    return new Vector3(
-      this.x * scalar,
-      this.y * scalar,
-      this.z * scalar
-    );
-  }
+    scale(scalar) {
+        return new Vector3(
+            this.x * scalar,
+            this.y * scalar,
+            this.z * scalar
+        );
+    }
 
-  plus(other) {
-    return new Vector3(
-      this.x + other.x,
-      this.y + other.y,
-      this.z + other.z
-    );
-  }
+    plus(other) {
+        return new Vector3(
+            this.x + other.x,
+            this.y + other.y,
+            this.z + other.z
+        );
+    }
 
-  minus(other) {
-    return new Vector3(
-      this.x - other.x,
-      this.y - other.y,
-      this.z - other.z
-    );
-  }
+    minus(other) {
+        return new Vector3(
+            this.x - other.x,
+            this.y - other.y,
+            this.z - other.z
+        );
+    }
 
-  static lerp(start, end, t) {
-    return start.scale(1 - t).plus(end.scale(t));
-  }
+    dot(other) {
+        return (
+            this.x * other.x +
+            this.y * other.y +
+            this.z * other.z
+        );
+    }
+
+    static lerp(start, end, t) {
+        return start.scale(1 - t).plus(end.scale(t));
+    }
 }
 
 class Ray {
-  constructor(origin, direction) {
-    this.origin = origin;
-    this.direction = direction;
-  }
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
 }
